2aab1bbc06e6e436723cdf90fec2567bb621a120
ccflack <ccflack@me.com>
2016-10-05 16:17:25 -0400

    Adventure mode completed

--- robots.rb
+++ robots.rb
@@ -3,10 +3,16 @@
 require 'erb'
 
 class Robot
-  attr_accessor :name
+  attr_accessor :name,
+                :height
 
-  def initialize(unit_name="001")
+  def initialize(unit_name="001", height="10")
     @name = unit_name
+    @height = height
+  end
+
+  def to_s
+    @name
   end
 
   def say_hi
@@ -38,6 +44,34 @@ class ActorUnit < Robot
   end
 
 end
+
+new_bender = BendingUnit.new(ARGV[0])
+unbent_thing = ARGV[1]
+
+#Adventure Mode
+
+def upgrade(group)
+  group.collect { |human| Robot.new("#{human} v2.0")}
+end
+
+our_class = %w(Luiz Zachary Chris Brent Keith Michael Jon)
+
+new_class = upgrade(our_class)
+
+# new_class = our_class.collect{|human| Robot.new("#{human} v2.0")}
+
+puts new_class
+
+#
+# new_class.each {|robot| puts robot.height}
+#
+def robot_roll_call(group)
+  group.each { |member| puts "#{member}, here!"}
+end
+
+robot_roll_call(new_class)
+
+new_class.each {|robot| puts "I am #{robot.height} units tall."}
 
 new_file = File.open("./#{ARGV[0]}.html", "w+")
 new_file << ERB.new(File.read("index.html.erb")).result(binding)
