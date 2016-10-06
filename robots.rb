ed29595040959cf1cd3a53c17b94f703cb060fec
ccflack <ccflack@me.com>
2016-10-05 22:12:53 -0400

    Added randomizer and random photo to bootstrap output.

--- robots.rb
+++ robots.rb
@@ -4,11 +4,13 @@ require 'erb'
 
 class Robot
   attr_accessor :name,
-                :height
+                :height,
+                :model
 
   def initialize(unit_name="001", height="10")
     @name = unit_name
     @height = height
+    @model = "Standard"
   end
 
   def to_s
@@ -29,6 +31,7 @@ class BendingUnit < Robot
 
   def initialize(unit_name="001-B")
     @name = unit_name
+    @model = "Bending"
   end
 
   def bend(object_to_bend)
@@ -41,6 +44,7 @@ class ActorUnit < Robot
 
   def change_name(new_name)
     @name = new_name
+    @model = "Actor"
   end
 
 end
@@ -52,18 +56,20 @@ unbent_thing = ARGV[1]
 
 our_class = %w(Luiz Zachary Chris Brent Keith Michael Jon)
 
+
 def upgrade(group)
-  group.collect { |human| Robot.new("#{human} v2.0")}
+  classes = [Robot, BendingUnit, ActorUnit]
+  group.collect { |human| classes.sample.new("#{human} v2.0")}
 end
 
 new_class = upgrade(our_class)
 
 def robot_roll_call(group)
-  group.each { |member| puts "#{member}, here! I am #{member.height} units tall. You will be upgraded."}
+  group.each { |member| puts "#{member}, here! I am #{member.model} unit. You will be upgraded."}
 end
 
 def solo_sound_off(group)
-  group.collect { |member| "#{member}, here! I am #{member.height} units tall. You will be upgraded." }
+  group.collect { |member| "#{member}, here! I am a #{member.model} model. You will be upgraded." }
 end
 
 calls = solo_sound_off(new_class)
@@ -72,15 +78,8 @@ robot_roll_call(new_class)
 
 new_class.each { |robot| puts "I am #{robot.height} units tall."}
 
-def height=
-
-end
-
-def height
-
-end
-
+puts calls[0]
 
 new_file = File.open("./#{ARGV[0]}.html", "w+")
-new_file << ERB.new(File.read("index.html.erb")).result(binding)
+new_file << ERB.new(File.read("test.html.erb")).result(binding)
 new_file.close
